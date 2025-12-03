eac5591e7f6d8b440df5bfb6e1f8000065aff7e1
Jeremy Guiselin <jeremy.guiselin@jobteaser.com>
2025-12-03 19:54:47 +0100

    feat: 2019 day 24

--- aoc-go/2019/24/day.go
+++ aoc-go/2019/24/day.go
@@ -3,85 +3,37 @@ package main
 import (
 	"embed"
 	"fmt"
-	"math"
 
+	"adventofcode-go/2019/24/internal"
 	"adventofcode-go/pkg/aocutils"
 )
 
 //go:embed input.txt
 var inputFile embed.FS
 
-func runPartOne() int {
-	seen := map[string]bool{}
-	space, maxX, maxY := parseInput()
-	for {
-		hash := toHash(space, maxX, maxY)
-		if _, ok := seen[hash]; ok {
-			fmt.Println(hash)
-			return biodiversityRate(hash)
-		}
-		seen[hash] = true
-		space = simulate(space)
-	}
-}
-
-func parseInput() (map[aocutils.Point]rune, int, int) {
+func parseInput() internal.Grid {
 	lines := aocutils.MustGetDayInput(inputFile)
 	space := map[aocutils.Point]rune{}
 	for y, line := range lines {
 		for x, r := range line {
-			space[aocutils.Point{X: x, Y: y}] = r
+			space[aocutils.Point{X: x, Y: y, Z: 0}] = r
 		}
 	}
-	return space, len(lines[0]) - 1, len(lines) - 1
+	return space
 }
 
-func toHash(space map[aocutils.Point]rune, maxX, maxY int) string {
-	spaceArray := make([][]rune, maxY+1)
-	for y := 0; y <= maxY; y++ {
-		spaceArray[y] = make([]rune, maxX+1)
-	}
-	hash := ""
-	for point, state := range space {
-		spaceArray[point.Y][point.X] = state
-	}
-	for _, row := range spaceArray {
-		hash += string(row)
-	}
-	return hash
+func runPartOne(grid internal.Grid) int {
+	space := internal.NewSimpleSpace(grid)
+	return space.Simulate()
 }
 
-func simulate(space map[aocutils.Point]rune) map[aocutils.Point]rune {
-	newSpace := map[aocutils.Point]rune{}
-	for point, state := range space {
-		neighbors := point.Neighbours2D(false)
-		numBugs := 0
-		for _, neighbor := range neighbors {
-			if space[neighbor] == '#' {
-				numBugs++
-			}
-		}
-		if state == '.' && (numBugs == 1 || numBugs == 2) {
-			newSpace[point] = '#'
-		} else if state == '#' && numBugs != 1 {
-			newSpace[point] = '.'
-		} else {
-			newSpace[point] = state
-		}
-	}
-	return newSpace
-}
-
-func biodiversityRate(repr string) int {
-	rate := 0
-	for x, r := range repr {
-		if r == '#' {
-			rate += int(math.Pow(2, float64(x)))
-		}
-	}
-	return rate
+func runPartTwo(grid internal.Grid) int {
+	space := internal.NewRecursiveSpace(grid, 200)
+	return space.Simulate()
 }
 
 func main() {
-	fmt.Println(runPartOne())
+	grid := parseInput()
+	fmt.Println(runPartOne(grid))
+	fmt.Println(runPartTwo(grid))
 }
